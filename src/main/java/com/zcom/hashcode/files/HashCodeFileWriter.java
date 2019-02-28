6f17151bd9537cabe1b2f1f3e369670f5a0d4fd0
unknown <e.k.makarovas@gmail.com>
2019-02-28 18:58:40 +0100

    .

--- src/main/java/com/zcom/hashcode/files/HashCodeFileWriter.java
+++ src/main/java/com/zcom/hashcode/files/HashCodeFileWriter.java
@@ -1,10 +1,10 @@
 package com.zcom.hashcode.files;
 
-import java.io.FileWriter;
+import com.zcom.hashcode.photo.Slideshow;
 
 public class HashCodeFileWriter {
 
-	public void writeToOutputFile(String filePath) {
+	public void writeToOutputFile(String filePath, Slideshow slideshow) {
 //		try(final FileWriter fw = new FileWriter(filePath)) {
 //			
 //		}
